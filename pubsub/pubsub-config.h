057c67d3a436849135824441be9f8d337f367d3f
Jon Gjengset <jon@thesquareplanet.com>
2013-04-07 18:53:21 +0100

    readings.h now a proper config

---
+++ pubsub/pubsub-config.h
@@ -0,0 +1,19 @@
+#include "math.h"
+
+enum reading_type {
+  READING_LOCATION,
+  READING_HUMIDITY,
+  READING_PRESSURE,
+};
+
+enum soft_filter  {
+  SOFT_FILTER_1
+};
+
+enum hard_filter {
+  HARD_FILTER_1
+};
+
+enum aggregator {
+  AGGREGATOR_1
+};
