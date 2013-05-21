a61c7821fe32aa0e1e22b888d27cc8c02a3ba2b0
Jon Gjengset <jon@thesquareplanet.com>
2013-05-21 11:59:06 +0100

    Implement BE_CLOSE_TO for van

--- pubsub/van.c
+++ pubsub/van.c
@@ -8,11 +8,12 @@
 #include "contiki.h"
 #include "lib/subscriber.h"
 #include "pubsub-config.h"
+#include "dev/serial-line.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 /*---------------------------------------------------------------------------*/
 #define MAX(a,b) (a>b?a:b)
-#define MIN_DEVIATION 1000
 /*---------------------------------------------------------------------------*/
 static void on_reading(subid_t subid, void *data) {
   const struct subscription *s = subscriber_subscription(subid);
@@ -38,28 +39,42 @@ AUTOSTART_PROCESSES(&van_process);
 PROCESS_THREAD(van_process, ev, data)
 {
   struct subscription s;
-  static struct location myloc = { 20, 10 };
+  static struct location l;
+  static bool gotx = false;
 
   PROCESS_BEGIN();
+  printf("acquiring position...\n");
+  while (1) {
+    // Wait for the next event.
+    PROCESS_YIELD_UNTIL(ev == serial_line_event_message);
+
+    if (!gotx) {
+      l.x = atoi((char*)data);
+      gotx = true;
+    } else {
+      l.y = atoi((char*)data);
+      break;
+    }
+  }
+  printf("location read: <%03d, %03d>\n", l.x, l.y);
 
   /* initialize subscriber */
   subscriber_start(&on_reading);
 
   /* no special stuff here */
-  s.soft.filter = DEVIATION;
-  s.soft.arg.deviation = MIN_DEVIATION;
+  s.soft.filter = NO_SOFT_FILTER;
   s.hard.filter = BE_CLOSE_TO;
-  s.hard.arg.loc = myloc;
+  s.hard.arg.loc = l;
   s.aggregator.aggregator = NO_AGGREGATION;
 
   /* subscribe to humidity */
-  s.interval = 10*CLOCK_SECOND;
+  s.interval = 5*CLOCK_SECOND;
   s.sensor = READING_HUMIDITY;
   subscriber_subscribe(&s);
   printf("subscribed to humidity\n");
 
   /* subscribe to pressure */
-  s.interval = 20*CLOCK_SECOND;
+  s.interval = 10*CLOCK_SECOND;
   s.sensor = READING_PRESSURE;
   subscriber_subscribe(&s);
   printf("subscribed to pressure\n");
